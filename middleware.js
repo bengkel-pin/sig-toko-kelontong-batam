eb0486084bddd3f32e2155783159e815b1db95cb
bengkel-pin <klvn2003@gmail.com>
2024-11-27 23:53:20 +0700

    Fixing error unprotected page

--- middleware.js
+++ middleware.js
@@ -3,13 +3,11 @@ import { decrypt } from './app/lib/session'
 import { cookies } from 'next/headers'
  
 // 1. Specify protected and public routes
-const protectedRoutes = ['/dashboard']
 const publicRoutes = ['/login']
  
 export default async function middleware(req) {
   // 2. Check if the current route is protected or public
   const path = req.nextUrl.pathname
-  const isProtectedRoute = protectedRoutes.includes(path)
   const isPublicRoute = publicRoutes.includes(path)
  
   // 3. Decrypt the session from the cookie
@@ -17,7 +15,7 @@ export default async function middleware(req) {
   const session = await decrypt(cookie)
  
   // 5. Redirect to /login if the user is not authenticated
-  if (isProtectedRoute && !session?.userId) {
+  if (req.nextUrl.pathname.startsWith('/dashboard') && !session?.userId) {
     return NextResponse.redirect(new URL('/login', req.nextUrl))
   }
  
